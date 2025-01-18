222e4d226b3210c2572e5bef585e22fa6f6118cd
blue <samuel_walter_@hotmail.com>
2025-01-17 22:02:47 -0500

    Refactor routes and add EnsureJsonResponse middleware

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
