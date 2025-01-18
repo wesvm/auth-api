c68a56c9ea3445a632798b1df24dbb5f9ef3d07c
blue <samuel_walter_@hotmail.com>
2025-01-17 22:37:14 -0500

    Add JwtRoleMiddleware for role-based API access control

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureJsonResponseMiddleware;
+use App\Http\Middleware\JwtRoleMiddleware;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -15,12 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [EnsureJsonResponseMiddleware::class]);
+        $middleware->alias(['role' => JwtRoleMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
-            return $request->expectsJson();
-        });
-
         $exceptions->render(function (ValidationException $exception) {
             return jsonResponse(
                 status: 422,
