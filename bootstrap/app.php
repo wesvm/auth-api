d3cf18384cc1775efcb0b663d0a0a9fe8aa1dd32
blue <samuel_walter_@hotmail.com>
2025-01-20 23:44:17 -0500

    Update JWT TTL and handle 404 exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -37,6 +38,10 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $exceptions->render(function (AuthenticationException $exception) {
             return jsonResponse(status: 401, message: $exception->getMessage());
+        });
+
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            return jsonResponse(status: 404, message: $exception->getMessage());
         });
 
         $exceptions->render(function (Throwable $exception) {
